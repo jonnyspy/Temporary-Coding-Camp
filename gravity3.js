c7a426d688a1943440d69c4c849f9ed7d2d4911a
Nathan Bunker <nbunker@immregistries.org>
2021-11-04 19:30:35 -0600

    Updated gravity3

--- gravity3.js
+++ gravity3.js
@@ -1,23 +1,5 @@
-void setup() {
-	  size(600,600);
-	}
 
-	void keyPressed() {
-		if (keyCode === RIGHT) {
-			playerXSpeed += 0.1;
-		} else if (keyCode === LEFT) {
-			playerXSpeed -= 0.1;
-		} else if (keyCode === UP) {
-			playerYSpeed -= 0.1;
-		} else if (keyCode === DOWN) {
-			playerYSpeed -= -0.1;
-		}
-	}
 	
-	var playerX = 200;
-	var playerY = 200;
-	var playerXSpeed = 0;
-	var playerYSpeed = 0;
 	
 	function drawPlayer() {
 		fill(0,0,0);
